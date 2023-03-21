d375f8564beab4211a6b220150b86a90a49c560a
Y4ROSLAV <jeros1337@gmail.com>
2023-03-21 20:26:31 +0300

    Refactor app: Modal in root #4

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import s from './App.module.css'
 import {Footer} from './components/Footer/Footer'
 import {Header} from './components/Header/Header'
 import {Catalog} from './components/Catalog/Catalog'
+import {Modal} from './components/Modal/Modal'
 
 function App() {
 	return (
 		<div className={s.Root}>
+			<Modal />
 			<Header />
 			<Catalog />
 			<Footer />
