38adebb70d3cc6ee214db88e5f7ed5e4846d9058
Y4ROSLAV <jeros1337@gmail.com>
2023-03-30 17:41:27 +0300

    Add App: react-router

--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,10 @@ import App from './App'
 import {store} from './redux/store'
 import {Provider} from 'react-redux'
 
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		// children: [
-		//   {
-		//     path: "items/:id",
-		//     element: <Team />,
-		//     loader: teamLoader,
-		//   },
-		// ],
-	},
-])
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<Provider store={store}>
-		<RouterProvider router={router} />
+		<App />
 	</Provider>,
 )
