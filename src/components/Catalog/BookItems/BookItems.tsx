25d44456a46e63ab07ae6e4e49ddd28b995c3645
Y4ROSLAV <jeros1337@gmail.com>
2023-04-04 22:22:42 +0300

    Refactor BookItem: make a filter function

--- src/components/Catalog/BookItems/BookItems.tsx
+++ src/components/Catalog/BookItems/BookItems.tsx
@@ -1,16 +1,21 @@
-import {Fragment} from 'react'
+import {useState} from 'react'
 import {BookItem} from './BookItem/BookItem'
 import s from './BookItems.module.css'
 import {useSelector, useDispatch} from 'react-redux'
 import {initializeBooks, selectBooks, selectSearchQuery} from 'reducers/books'
 import {useEffect} from 'react'
 import {getBooksLS, setBooksLS} from 'src/localStorageInteraction'
+import {getFilteredBooks} from 'src/utils/getFilteredBooks'
 
 export const BookItems = () => {
 	const books = useSelector(selectBooks)
-	const dispatch = useDispatch()
-
 	const query = useSelector(selectSearchQuery)
+
+	const getDefaultFiltered = getFilteredBooks(books, query, query, 'authorOrTitle')
+
+	const [filteredBooks, setFilteredBooks] = useState(getDefaultFiltered)
+
+	const dispatch = useDispatch()
 
 	useEffect(() => {
 		// инициализация книжек
@@ -24,29 +29,34 @@ export const BookItems = () => {
 		setBooksLS(books)
 	}, [books])
 
+	useEffect(() => {
+		setFilteredBooks(getDefaultFiltered)
+	}, [books, query, getDefaultFiltered])
+
 	return (
 		<div className={s.Root}>
-			{books.length === 0 && (
-				<div className={s.warning}>There are no books in the catalog here yet.</div>
-			)}
-			{books.map((book) => {
-				if (book.author.includes(query) || book.title.includes(query)) {
-					return (
-						<BookItem
-							key={book.id}
-							title={book.title}
-							id={book.id}
-							ISBN={book.ISBN}
-							author={book.author}
-							description={book.description}
-							imgSrc={book.imgSrc}
-							publishYear={book.publishYear}
-							publisher={book.publisher}
-							format='icon'
-						/>
-					)
-				}
-				return <Fragment key={book.id} />
+			{filteredBooks.length === 0 &&
+				(books.length > 0 ? (
+					<div className={s.warning}>Nothing was found for your query.</div>
+				) : (
+					<div className={s.warning}>There are no books in the catalog here yet.</div>
+				))}
+
+			{filteredBooks.map((book) => {
+				return (
+					<BookItem
+						key={book.id}
+						title={book.title}
+						id={book.id}
+						ISBN={book.ISBN}
+						author={book.author}
+						description={book.description}
+						imgSrc={book.imgSrc}
+						publishYear={book.publishYear}
+						publisher={book.publisher}
+						format='icon'
+					/>
+				)
 			})}
 		</div>
 	)
---
+++ src/utils/getFilteredBooks.ts
@@ -0,0 +1,41 @@
+import {BookItemType} from 'components/Catalog/BookItems/BookItem/BookItem'
+import {exhaustiveCheck} from './functions'
+
+type filterFieldType = '' | 'author' | 'title' | 'authorOrTitle' | 'authorAndTitle'
+
+export const getFilteredBooks = (
+	books: Array<BookItemType>,
+	authorQuery = '',
+	titleQuery = '',
+	filterField: filterFieldType,
+) => {
+	authorQuery = authorQuery.toLowerCase()
+	titleQuery = titleQuery.toLowerCase()
+
+	if (filterField === '') {
+		return books
+	}
+
+	const filteredBooks = books.filter((book) => {
+		const bookAuthorContains = book.author.toLowerCase().includes(authorQuery)
+		const bookTitleContains = book.title.toLowerCase().includes(titleQuery)
+
+		// console.log(bookAuthorContains, bookTitleContains)
+		// console.log(book.author, authorQuery, book.title, titleQuery)
+
+		switch (filterField) {
+			case 'author':
+				return bookAuthorContains
+			case 'title':
+				return bookTitleContains
+			case 'authorOrTitle':
+				return bookAuthorContains || bookTitleContains
+			case 'authorAndTitle':
+				return bookAuthorContains && bookTitleContains
+			default:
+				return exhaustiveCheck(filterField)
+		}
+	})
+	console.log(filteredBooks)
+	return filteredBooks
+}
