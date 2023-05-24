aa18862783f1d1ce58bd14cb47315c2bc5d9d356
Y4ROSLAV <jeros1337@gmail.com>
2023-05-24 13:55:37 +0300

    merge: feature 5 into 4

--- src/components/Catalog/SearchInput/SearchInput.tsx
+++ src/components/Catalog/SearchInput/SearchInput.tsx
@@ -1,37 +1,4 @@
 import s from './SearchInput.module.css'
-import {useDispatch, useSelector} from 'react-redux'
-import {selectSearchQuery, setSearchQuery} from 'reducers/books'
-import {useSearchParams} from 'react-router-dom'
-import {useEffect} from 'react'
-
-export const SearchInput = () => {
-	const query = useSelector(selectSearchQuery)
-	const dispatch = useDispatch()
-
-	const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const search = e.currentTarget.value
-		if (!search) {
-			return setSearchParams({})
-		}
-		setSearchParams({search})
-	}
-
-	const [searchParams, setSearchParams] = useSearchParams()
-
-	useEffect(() => {
-		dispatch(setSearchQuery(searchParams.get('search') || ''))
-	}, [dispatch, searchParams])
-
-	return (
-		<input
-			className={s.Root}
-			type='text'
-			value={query}
-			onChange={(e) => onChangeHandler(e)}
-		/>
-	)
-}
-import s from './SearchInput.module.css'
 import {useDispatch} from 'react-redux'
 import {useSearchParams} from 'react-router-dom'
 import {useEffect, useState} from 'react'
