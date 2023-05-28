aca6e1beade8fd1e2c956a91b54f7fbbc4bfbd61
Y4ROSLAV <jeros1337@gmail.com>
2023-05-28 15:56:48 +0300

    Change styles: dark theme mode

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -2,11 +2,15 @@ import {Link} from 'react-router-dom'
 import {ToggleThemeMode} from 'src/components/ToggleThemeMode/ToggleThemeMode'
 
 import s from './Header.module.css'
+import {useSelector} from 'react-redux'
+import {selectSearchQuery} from 'reducers/books'
 
 export const Header = () => {
+	const searchQuery = useSelector(selectSearchQuery)
+
 	return (
 		<div className={s.Root}>
-			<Link to={'..'}>Books for everyone</Link>
+			<Link to={searchQuery ? `./?search=${searchQuery}` : '..'}>Books for everyone</Link>
 			<ToggleThemeMode />
 		</div>
 	)
