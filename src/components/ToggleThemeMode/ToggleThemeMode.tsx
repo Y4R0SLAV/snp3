d7ee4e2f84c2db3c9adb5212fcb52b8071e9548c
Y4ROSLAV <jeros1337@gmail.com>
2023-05-28 22:39:01 +0300

    Add theme: current theme mode button now highlighted

--- src/components/ToggleThemeMode/ToggleThemeMode.tsx
+++ src/components/ToggleThemeMode/ToggleThemeMode.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import {Button} from 'common/Button/Button'
 import {useThemeMode, Themes} from 'src/hooks/useThemeMode'
 import classNames from 'classnames/bind'
@@ -5,28 +6,40 @@ import classNames from 'classnames/bind'
 import s from './ToggleThemeMode.module.css'
 
 export const ToggleThemeMode = () => {
-	const {supportedThemes, setTheme} = useThemeMode()
+	const {theme, supportedThemes, setTheme} = useThemeMode()
+	const [currentThemeMode, setCurrentThemeMode] = useState<Themes>(theme)
+
 	const cx = classNames.bind(s)
 
 	const handleDarkThemeClick = () => {
 		setTheme(supportedThemes.dark as Themes)
+		setCurrentThemeMode(supportedThemes.dark as Themes)
 	}
 
 	const handleLightThemeClick = () => {
 		setTheme(supportedThemes.light as Themes)
+		setCurrentThemeMode(supportedThemes.light as Themes)
 	}
 
 	return (
 		<div className={s.Root}>
 			<Button
-				classNames={cx({button: true, button1: true})}
+				classNames={cx({
+					button: true,
+					button1: true,
+					active: currentThemeMode === supportedThemes.light,
+				})}
 				onClick={handleLightThemeClick}
 			>
 				Light
 			</Button>
 
 			<Button
-				classNames={cx({button: true, button2: true})}
+				classNames={cx({
+					button: true,
+					button2: true,
+					active: currentThemeMode === supportedThemes.dark,
+				})}
 				onClick={handleDarkThemeClick}
 			>
 				Dark
